e9dfa0ddea59c387b7f276640f6a9a9efe5fe73e
Lucas Dinnouti <lucas.dinnouti@wavy.global>
2020-07-21 22:34:20 -0300

    Remove unused code

--- src/br/unicamp/hero/quest/model/characters/Character.java
+++ src/br/unicamp/hero/quest/model/characters/Character.java
@@ -42,32 +42,5 @@ public abstract class Character {
         }
     }
 
-    public Action getRoundAction() {
-        return new Action() {
-            @Override
-            public int hashCode() {
-                return super.hashCode();
-            }
-
-            @Override
-            public boolean equals(Object obj) {
-                return super.equals(obj);
-            }
-
-            @Override
-            protected Object clone() throws CloneNotSupportedException {
-                return super.clone();
-            }
-
-            @Override
-            public String toString() {
-                return super.toString();
-            }
-
-            @Override
-            protected void finalize() throws Throwable {
-                super.finalize();
-            }
-        };
-    }
+    public abstract Action getRoundAction();
 }
