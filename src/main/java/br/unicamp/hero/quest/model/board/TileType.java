95602663906f710dfd862ed153aa5ab3a1d500a1
Lucas Dinnouti <lucas.dinnouti@wavy.global>
2020-07-23 20:47:58 -0300

    Add representations to tiles

--- src/main/java/br/unicamp/hero/quest/model/board/TileType.java
+++ src/main/java/br/unicamp/hero/quest/model/board/TileType.java
@@ -1,6 +1,18 @@
 package br.unicamp.hero.quest.model.board;
 
 public enum TileType {
-    WALL,
-    PATH
+    WALL("XX"),
+    PATH("  "),
+    SHADOW("::"),
+    EMPTY("  ");
+
+    private final String representation;
+
+    TileType(String representation) {
+        this.representation = representation;
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
 }
