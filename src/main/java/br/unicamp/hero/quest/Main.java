5c423a84efd0d46f8b0033630f55ebc63eed814c
Igormandello <igormandello@gmail.com>
2020-07-23 21:38:19 -0300

    Adds TerminalRenderService to main

--- src/main/java/br/unicamp/hero/quest/Main.java
+++ src/main/java/br/unicamp/hero/quest/Main.java
@@ -2,12 +2,15 @@ package br.unicamp.hero.quest;
 
 import br.unicamp.hero.quest.model.board.*;
 import br.unicamp.hero.quest.model.characters.hero.*;
+import br.unicamp.hero.quest.service.render.*;
 
 public class Main {
     public static void main(String[] args) {
         final Hero hero = new Barbarian(2, 1);
 
         final Board board = new Board(hero);
-        System.out.println(board.toString());
+        final RenderService renderService = new TerminalRenderService();
+
+        renderService.render(board);
     }
 }
