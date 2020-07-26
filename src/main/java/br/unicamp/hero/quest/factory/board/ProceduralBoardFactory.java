90f51122486dd64d7ca9780e0bc6bf88bfd4ef15
Igormandello <igormandello@gmail.com>
2020-07-26 17:49:56 -0300

    Adds procedural board factory base

---
+++ src/main/java/br/unicamp/hero/quest/factory/board/ProceduralBoardFactory.java
@@ -0,0 +1,10 @@
+package br.unicamp.hero.quest.factory.board;
+
+import br.unicamp.hero.quest.model.board.*;
+
+public class ProceduralBoardFactory implements BoardFactory {
+    @Override
+    public Board getBoard() {
+        throw new UnsupportedOperationException();
+    }
+}
