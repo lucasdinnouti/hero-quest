92e84820c6c5889b95f011dfc4abd076092bdab1
Igormandello <igormandello@gmail.com>
2020-07-26 18:43:37 -0300

    Adds canvas render service base

---
+++ src/main/java/br/unicamp/hero/quest/service/render/CanvasRenderService.java
@@ -0,0 +1,15 @@
+package br.unicamp.hero.quest.service.render;
+
+import br.unicamp.hero.quest.model.board.*;
+
+public class CanvasRenderService implements RenderService {
+    @Override
+    public void render(Board board) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void displayMessage(String message) {
+        throw new UnsupportedOperationException();
+    }
+}
